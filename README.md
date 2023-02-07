f8363783eeb4a254d5ff136dfa5465d91d488d43
vsulimovvv <super.sulimov2014@yandex.ru>
2023-02-08 02:00:25 +0300

    init project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Decor City Group
+
+### Страницы
+
+Главная: https://vsulimovvv.github.io/dealers/app/index.html  
+Маркизы терассного типа: https://vsulimovvv.github.io/dealers/app/terrace.html  
+Маркизы оконные: https://vsulimovvv.github.io/dealers/app/window.html  
+Маркизы корзинные: https://vsulimovvv.github.io/dealers/app/basket.html  
+Натяжные тенты: https://vsulimovvv.github.io/dealers/app/tents.html  
+Перголы: https://vsulimovvv.github.io/dealers/app/pergolas.html  
+Уличные зонты: https://vsulimovvv.github.io/dealers/app/umbrellas.html  
+Самолёты: https://vsulimovvv.github.io/dealers/app/aircrafts.html  
+Автоматика: https://vsulimovvv.github.io/dealers/app/automation.html  
---
+++ app/js/main.js
@@ -0,0 +1,113 @@
+window.addEventListener('DOMContentLoaded', () => {
+  //   // * ===== Mask input
+  //   $('input[type="tel"]').mask('+7 (999) 999-99-99');
+
+  //   // * ===== Nice Select
+  //   // $('select').niceSelect();
+
+  // * ===== Accordion
+  const toggleAccordion = (accordionControl, accordionContent, accordion) => {
+    const filters = document.querySelectorAll(accordionControl);
+    filters.forEach((el) => {
+      if (el) {
+        el.addEventListener('click', (e) => {
+          const target = e.target.closest(accordion);
+          const content = target.querySelector(accordionContent);
+          target.classList.toggle('active');
+          if (target.classList.contains('active')) {
+            content.style.maxHeight = content.scrollHeight + 'px';
+          } else {
+            content.style.maxHeight = null;
+          }
+        });
+      }
+    });
+  };
+  toggleAccordion('.accordion-control', '.accordion-content', '.accordion');
+
+  // * ===== Slider
+  (function slider() {
+    const sliderEl = document.querySelectorAll('.section-slider__slider');
+
+    sliderEl.forEach((el) => {
+      if (el) {
+        new Swiper(el, {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          navigation: {
+            nextEl: '.section-slider__slider .swiper-button-next',
+            prevEl: '.section-slider__slider .swiper-button-prev',
+          },
+        });
+      }
+    });
+  })();
+
+  // * ===== Fixed Header
+  (function fixedHeader() {
+    function scrollHeader() {
+      const nav = document.querySelector('header');
+      if (this.scrollY >= 50) {
+        nav.classList.add('scroll-header');
+      } else {
+        nav.classList.remove('scroll-header');
+      }
+    }
+    window.addEventListener('scroll', scrollHeader);
+    // ! Change
+    function changeBg() {
+      const header = document.querySelector('header');
+      if (window.pageYOffset >= 50) {
+        header.classList.add('scroll-header');
+      }
+    }
+    changeBg();
+  })();
+
+  // * ===== Show Menu
+  (function showMenu() {
+    const menuBtn = document.querySelector('.header__toggle');
+    const menu = document.querySelector('.mobile-menu');
+    const body = document.querySelector('body');
+    // const overlay = document.querySelector('.overlay');
+    menuBtn.addEventListener('click', (e) => {
+      menu.classList.toggle('active');
+      menuBtn.classList.toggle('active');
+      body.classList.toggle('no-scroll');
+    });
+  })();
+
+  // * ===== Modal
+  (function modals() {
+    function bindModal(openBtn, modal, close) {
+      const openBtnEl = document.querySelectorAll(openBtn);
+      const modalEl = document.querySelector(modal);
+      const closeEl = document.querySelectorAll(close);
+      const body = document.querySelector('body');
+      if (modalEl) {
+        openBtnEl.forEach((el) => {
+          el.addEventListener('click', (e) => {
+            if (e.target) {
+              e.preventDefault();
+            }
+            modalEl.classList.add('active');
+            body.classList.add('no-scroll');
+          });
+        });
+        closeEl.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          });
+        });
+        modalEl.addEventListener('click', (e) => {
+          if (e.target === modalEl) {
+            modalEl.classList.remove('active');
+            body.classList.remove('no-scroll');
+          }
+        });
+      }
+    }
+    bindModal('.btn-consulation', '.popup--consulation', '.popup__close');
+  })();
+});
